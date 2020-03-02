7d0dab8c1e32f897b7aae746a81b87400bbfea18
kobe-gabriel <gabriel.machado@kobe.io>
2020-03-02 17:52:45 -0300

    refactor: use es6 async/await promises

--- src/index.js
+++ src/index.js
@@ -1,59 +1,109 @@
 const admin = require('firebase-admin');
-const fs = require('fs');
 const path = require('path');
 const os = require('os');
-const { exec } = require('child_process');
-const zipFolder = require('zip-folder');
+const util = require('util');
+const access = util.promisify(require('fs').access);
+const exec = util.promisify(require('child_process').exec);
+const { zip } = require('zip-a-folder');
 
-exports.runBackup = (serviceAccountPath, bucketURL) => {
+async function isRequestValid(serviceAccountPath, bucketURL) {
   if (!serviceAccountPath) {
-    return { status: false, message: 'Service Account JSON path not provided' };
+    return false;
   }
 
   if (!bucketURL) {
-    return { status: false, message: 'Bucket URL not provided' };
+    return false;
+  }
+
+  return true;
+}
+
+async function isPathAccessible(dir) {
+  try {
+    const { err } = access(dir);
+    if (err) {
+      return false;
+    }
+
+    return true;
+  } catch (err) {
+    return false;
+  }
+}
+
+async function createTemporaryFolder(dir) {
+  try {
+    const { stderr } = await exec(`mkdir ${dir}`);
+
+    if (stderr) {
+      return false;
+    }
+
+    return true;
+  } catch (err) {
+    return false;
+  }
+}
+
+async function runBackupTool(serviceAccountPath, tempFilePath) {
+  try {
+    const cmd = `npx firestore-backup-restore -a ${serviceAccountPath} -B ${tempFilePath}`;
+    const { stdout, stderr } = await exec(cmd, { cwd: __dirname });
+
+    if (stdout.match(/Backing up Document/) && stdout.match(/Restoring to collection/)) {
+      return true;
+    }
+
+    console.error('stdout: ', stdout);
+    console.error('stderr', stderr);
+    return false;
+  } catch (err) {
+    console.log(err);
+    return false;
+  }
+}
+
+module.exports = async (serviceAccountPath, bucketURL) => {
+  if (!isRequestValid(serviceAccountPath, bucketURL)) {
+    return { status: true, message: 'Invalid params' };
   }
 
   const dir = path.join(os.tmpdir(), 'backups');
 
-  fs.stat(dir, (err) => {
-    if (err) {
-      exec(`mkdir ${dir}`, (err) => {
-        if (err) {
-          return { status: false, message: `Error while creating temporary folder: ${err}` };
-        }
-      });
+  if (!await isPathAccessible(dir)) {
+    if (!await createTemporaryFolder(dir)) {
+      return { status: false, message: 'Error while creating temporary folder' };
     }
-  });
+  }
 
   const dateNow = new Date();
   const fileName = dateNow.toISOString();
-
   const tempFilePath = path.join(dir, fileName);
 
-  exec(`firestore-backup-restore -a ${serviceAccountPath} -B ${tempFilePath}`, (err) => {
-    if (err) {
-      return { status: false, message: `The backup has failed: ${err}` };
-    }
+  if (!await runBackupTool(serviceAccountPath, tempFilePath)) {
+    return { status: false, message: 'The backup has failed' };
+  }
 
-    const zipFilePath = path.join(dir, `${fileName}.zip`);
+  const zipFilePath = path.join(dir, `${fileName}.zip`);
 
-    zipFolder(tempFilePath, zipFilePath, (err) => {
-      if (err) {
-        return { status: false, message: `Error while zipping backup directory: ${err}` };
-      }
+  const err = await zip(tempFilePath, zipFilePath);
+  if (err) {
+    return { status: false, message: `Error while zipping backup directory: ${err}` };
+  }
 
-      admin.initializeApp({
-        credential: admin.credential.cert(serviceAccountPath),
-      });
+  const serviceAccount = require(serviceAccountPath); //eslint-disable-line
 
-      const bucket = admin.storage().bucket(bucketURL);
-
-      bucket.upload(zipFilePath, {
-        destination: path.join('backups', fileName),
-      })
-        .then(() => ({ status: true, message: 'Backup completed and uploaded to storage in backups folder' }))
-        .catch((err) => ({ status: false, message: `Error while uploading to storage: ${err}` }));
-    });
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
   });
+
+  const bucket = admin.storage().bucket(bucketURL);
+
+  const options = {
+    destination: path.join('backups', fileName),
+  };
+
+  await bucket.upload(zipFilePath, options);
+
+  return { status: true, message: 'Backup completed and uploaded to storage in backups folder' };
 };
